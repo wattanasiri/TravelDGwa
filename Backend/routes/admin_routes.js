6a5d04cd7664b494a17f19f9c90df7eb2a9772ca
Phongrawit <phongrawit_2544@hotmail.com>
2022-05-09 22:33:28 +0700

    deploy on heroku

--- Backend/routes/admin_routes.js
+++ Backend/routes/admin_routes.js
@@ -2,6 +2,7 @@ const express  = require('express');
 const User = require('../models/user_model')
 const passport = require('passport')
 const router = express.Router()
+const rentcarpartner = require('../models/rentcar_partner_model')
 
 router.post('/signin',(req,res,next) => {
     passport.authenticate('local', function(err, user, info) {
@@ -58,6 +59,28 @@ router.post('/promote/:id',(req,res) => {
             console.log(err)
         } else {
             return res.json(foundUser);
+        }
+    })
+})
+
+router.get('/getrentcarpartner',(req,res) => {
+    rentcarpartner.find().populate('usernameID').exec((err,foundPartner) => {
+        if(err){
+            console.log(err)
+        } else{
+            console.log(foundPartner);
+            return res.json(foundPartner);
+        }
+    })
+})
+
+router.get('/getrentcarpartner/:id',(req,res) => {
+    rentcarpartner.findById(req.params.id).populate('usernameID').exec((err,foundPartner) => {
+        if(err){
+            console.log(err)
+        } else{
+            console.log(foundPartner);
+            return res.json(foundPartner);
         }
     })
 })
